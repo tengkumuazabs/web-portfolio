5538709456225c932569fb7b9791bb968e977d2e
tengkumuazabs <muaz.zero@gmail.com>
2026-05-03 21:19:31 +0700

    update app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import { Terminal } from "lucide-react";
 const projects = [
   {
     name: "COVID-19 Spatial Analysis",
-    link: "https://covid-19-data-visualizer.streamlit.app",
+    link: "https://github.com/tengkumuazabs/covid-19-dashboard",
   },
   {
     name: "Sentiment Analysis of Temple Reviews",
